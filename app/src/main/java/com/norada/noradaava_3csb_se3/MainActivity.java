e46e00c4908b8a46e79b9b179725237de48e26dd
Ava Norada <avajulia.norada.iics@ust.edu.ph>
2020-11-25 16:55:29 +0800

    second commit Norada

--- app/src/main/java/com/norada/noradaava_3csb_se3/MainActivity.java
+++ app/src/main/java/com/norada/noradaava_3csb_se3/MainActivity.java
@@ -6,22 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+    SharedPreferences sharedPreferences;
     ListView listView;
     String[] collegeNames = {"Inst. of Information and Computing Sciences", "Faculty of Engineering", "College of Architecture"};
     String[] patronSaint = {"St. Vincent Ferrer", "Blessed Jordan of Saxony", "Jordan the Apostle"};
     String[] foundedDate = {"Founded in 2014", "Founded 1907", "Founded 1930"};
     int[] collegeIcons = {R.drawable.iics, R.drawable.eng, R.drawable.arki};
+
+    String cName="";
+    String cSaint="";
+    String cYear="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         listView =findViewById(R.id.listView);
         MyAdapter adapter = new MyAdapter(this, collegeNames, patronSaint, foundedDate, collegeIcons);
         listView.setAdapter(adapter);
-
+        sharedPreferences = getSharedPreferences("user", Context.MODE_PRIVATE);
     }
 
     class MyAdapter extends ArrayAdapter<String>{
@@ -65,8 +72,26 @@ public class MainActivity extends AppCompatActivity {
             tvNames.setText(rCollegeNames[position]);
             tvSaint.setText(rPatronSaint[position]);
             tvDate.setText(rFoundedDate[position]);
-
+            ///adapter= new CustomAdapter(this);
             return row;
         }
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        cName=collegeNames[position];
+        cSaint=patronSaint[position];
+        cYear=foundedDate[position];
+
+   }
+
+    public void saveShare(View view){
+        SharedPreferences.Editor editor= sharedPreferences.edit();
+
+        editor.putString("collegeName",cName);
+        editor.putString("collegeSaint",cSaint);
+        editor.putString("collegeYear",cYear);
+        editor.commit();
+
+        Toast.makeText(this,"Saved data in shared preferences!",Toast.LENGTH_LONG).show();
     }
 }
